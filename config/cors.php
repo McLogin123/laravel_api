1ac322a0cc7aae52145a7503748523e9e69880f3
McLogin123 <103536901+McLogin123@users.noreply.github.com>
2025-11-30 18:16:13 -0300

    Add files via upload

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['react-app-f1.vercel.app'],
+    'allowed_origins' => ['https://react-app-f1.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['*'],
